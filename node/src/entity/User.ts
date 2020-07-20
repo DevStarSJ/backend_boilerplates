3a77d0797fbd9c05ad11485afbf066babb622207
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-20 15:40:57 +0900

    [node] migration 코드 정리

--- node/src/entity/User.ts
+++ node/src/entity/User.ts
@@ -17,28 +17,3 @@ export class User {
     @UpdateDateColumn({name: 'updated_at',type: "timestamp"})
     public updatedAt: Date;
 }
-
-
-/*
-
-import {MigrationInterface, QueryRunner} from "typeorm";
-
-export class createUser1593410722984 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-        CREATE TABLE users (
-            id int(11) PRIMARY KEY AUTO_INCREMENT,
-            username varchar(255) NOT NULL,
-            password varchar(255) NOT NULL,
-            created_at timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-            updated_at timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
-          )`);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE users if exists`);
-    }
-}
-
-*/
--- node/src/migration/1593412271017-createUser.ts
+++ node/src/migration/1593412271017-createUser.ts
@@ -4,11 +4,18 @@ export class createUser1593412271017 implements MigrationInterface {
     name = 'createUser1593412271017'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE TABLE `users` (`id` int NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL, `password` varchar(255) NOT NULL, `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query(`
+        CREATE TABLE users (
+            id int(11) PRIMARY KEY AUTO_INCREMENT,
+            username varchar(255) NOT NULL,
+            password varchar(255) NOT NULL,
+            created_at timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+            updated_at timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
+          )`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DROP TABLE `users`");
+        await queryRunner.query("DROP TABLE `users` if exists");
     }
 
 }
