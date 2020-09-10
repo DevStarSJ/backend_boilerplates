f2871d7630d3cce2a682979bf6a3643cc45e700b
Yun Seokjoon <seokjoon.yun@gmail.com>
2020-09-10 15:27:16 +0900

    [Nest.JS] loggly 깔끔하게

--- nest.js/src/main.ts
+++ nest.js/src/main.ts
@@ -7,44 +7,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 // .env 파일에 영향을 받는 코드들은 이후에 선언되어야 함
-
-require('newrelic')
-import { Loggly } from 'winston-loggly-bulk'
-// const Loggly = require('winston-loggly-bulk').Loggly
-import winston from 'winston'
-import morgan from 'morgan'
-import expressWinston from 'express-winston'
-
-const logger = expressWinston.logger({
-  transports: [
-    new Loggly({
-      token: process.env.LOGGLY_KEY || '',
-      subdomain: 'genoplan',
-      tags: ['NestJS'],
-      json: true
-    }),
-    new winston.transports.Console({ 
-      level: 'info'
-    })
-  ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
-  meta: true,
-  expressFormat: true,
-})
-expressWinston.requestWhitelist.push('body')
-expressWinston.responseWhitelist.push('body')
-
-// logger.stream = {
-//   write: (info) => {
-//     logger.info(info)
-//   }
-// }
-
-import log from './middlewares/log'
-import { LoggingInterceptor } from './middlewares/logging.interceptor'
+import { logger, errorLogger } from './middlewares/logger.middleware'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -65,15 +28,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('docs', app, document)
 
-  // app.use(log)
-  // app.useGlobalInterceptors(new LoggingInterceptor())
-
-  // app.use(morgan('combined', { stream: {
-  //   write: (info) => {
-  //     logger.info(info)
-  //   }
-  // }}))
   app.use(logger)
+  app.use(errorLogger)
   await app.listen(3000)
 }
 bootstrap()
---
+++ nest.js/src/middlewares/logger.middleware.ts
@@ -0,0 +1,27 @@
+require('newrelic')
+import { Loggly } from 'winston-loggly-bulk'
+import winston from 'winston'
+import expressWinston from 'express-winston'
+
+expressWinston.requestWhitelist.push('body')
+expressWinston.responseWhitelist.push('body')
+
+const option = {
+  transports: [
+    new Loggly({
+      token: process.env.LOGGLY_KEY || '',
+      subdomain: 'genoplan',
+      tags: ['NestJS'],
+      json: true
+    }),
+    new winston.transports.Console({ 
+      level: 'info'
+    })
+  ],
+  format: winston.format.json(),
+  meta: true,
+  expressFormat: true,
+}
+
+export const logger = expressWinston.logger(option)
+export const errorLogger = expressWinston.errorLogger(option)
