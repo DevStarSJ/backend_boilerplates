65edaab0355f9f3070a1c9c493c38ede07ca4dcd
seokjoon.yun <seokjoon.yun@gmail.com>
2020-11-30 11:17:17 +0900

    [rails][SocialLogin] Refactoring

--- rails/app/services/social_auth_service.rb
+++ rails/app/services/social_auth_service.rb
@@ -1,88 +1,43 @@
 class SocialAuthService
   def self.google(params)
-    return nil unless params["provider"].present? && params["uid"].present? && params["info"].present?
-    return nil unless params["provider"].include?('google')
-
-    social_auth = SocialAuth.where(provider: 'google', uid: params["uid"]).first_or_create do |auth|
-      auth.provider = 'google'
-      auth.uid = params["uid"]
-      auth.email = params["info"]["email"]
-      auth.first_name = params["info"]["first_name"]
-      auth.last_name = params["info"]["last_name"]
-      auth.photo = params["info"]["image"]
-
-      link_user(auth)
-    end
-
-    social_auth.user
+    apply(params, 'google')
   end
 
   def self.facebook(params)
-    return nil unless params["provider"].present? && params["uid"].present? && params["info"].present?
-    return nil unless params["provider"] == 'facebook'
-
-    social_auth = SocialAuth.where(provider: params["provider"], uid: params["uid"]).first_or_create do |auth|
-      auth.provider = params["provider"]
-      auth.uid = params["uid"]
-      auth.email = params["info"]["email"]
-      auth.first_name = params["info"]["name"]
-      auth.photo = params["info"]["image"]
-
-      link_user(auth)
-    end
-
-    social_auth.user
+    apply(params, 'facebook')
   end
 
   def self.naver(params)
-    return nil unless params["provider"].present? && params["uid"].present? && params["info"].present?
-    return nil unless params["provider"] == 'naver'
-
-    social_auth = SocialAuth.where(provider: params["provider"], uid: params["uid"]).first_or_create do |auth|
-      auth.provider = params["provider"]
-      auth.uid = params["uid"]
-      auth.email = params["info"]["email"]
-      auth.first_name = params["info"]["name"]
-      auth.photo = params["info"]["image"]
-
-      link_user(auth)
-    end
-
-    social_auth.user
+    apply(params, 'naver')
   end
 
   def self.line(params)
-    return nil unless params["provider"].present? && params["uid"].present? && params["info"].present?
-    return nil unless params["provider"] == 'line'
-
-    social_auth = SocialAuth.where(provider: params["provider"], uid: params["uid"]).first_or_create do |auth|
-      auth.provider = params["provider"]
-      auth.uid = params["uid"]
-      auth.email = params["info"]["email"]
-      auth.first_name = params["info"]["name"]
-      auth.photo = params["info"]["image"]
-
-      link_user(auth)
-    end
-
-    social_auth.user
+    apply(params, 'line')
   end
 
   def self.kakao(params)
-    return nil unless params["provider"].present? && params["uid"].present? && params["info"].present?
-    return nil unless params["provider"] == 'kakao'
+    apply(params, 'kakao')
+  end
 
-    social_auth = SocialAuth.where(provider: params["provider"], uid: params["uid"]).first_or_create do |auth|
-      auth.provider = params["provider"]
+  def self.apply(params, provider)
+    return nil unless params["provider"].present? && params["uid"].present? && params["info"].present?
+    return nil unless params["provider"].include?(provider)
+
+    social_auth = initialize(params, provider)
+    social_auth.user
+  end
+
+  def self.initialize(params, provider)
+    SocialAuth.where(provider: provider, uid: params["uid"]).first_or_create do |auth|
+      auth.provider = provider
       auth.uid = params["uid"]
       auth.email = params["info"]["email"]
-      auth.first_name = params["info"]["name"]
+      auth.first_name = params["info"]["first_name"] ||  params["info"]["name"]
+      auth.last_name = params["info"]["last_name"]
       auth.photo = params["info"]["image"]
-
+  
       link_user(auth)
     end
-
-    social_auth.user
   end
 
   def self.link_user(auth)
