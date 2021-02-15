88a00b264f849b9334440e9fde674b2e1c2daf1d
seokjoon.yun <seokjoon.yun@gmail.com>
2021-02-16 07:16:09 +0900

    [nest-dynamo] lambda에 s3 full access 부여, lambda에서 credential 설정 안하도록 변경

--- nest.js-serverless-dynamodb/src/board/file.service.ts
+++ nest.js-serverless-dynamodb/src/board/file.service.ts
@@ -7,10 +7,13 @@ export class FilesService {
 
   constructor() {
     this.s3 = new AWS.S3()
-    AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-    })
+
+    if (process.env.AWS_ACCESS_KEY &&  process.env.AWS_SECRET_KEY && process.env.AWS_ACCESS_KEY.length > 0 && process.env.AWS_SECRET_KEY.length > 0) {
+      AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      })  
+    }
   }
  
   async uploadPublicFile(dataBuffer: Buffer, category: string, filename: string) {
